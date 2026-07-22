8db0a0519661165262261c66a8a597df92e4636b
iower <iower@tutanota.com>
2026-07-22 14:28:33 +0500

    vars 2

--- 03-vars.c
+++ 03-vars.c
@@ -1,2 +1,23 @@
+#include <stdio.h>
+
 int number;
 int _number33;
+int Number;
+
+int number1, number2, number3;
+
+int main(void)
+{
+	int number;
+	number = 1;
+	printf("number = %d\n", number);
+
+	int number2 = 2;
+	printf("number2 = %d\n", number2);
+
+	int n1 = 1, n2 = 2, n3 = 3;
+
+	printf("n1, n2, n3 = %d, %d, %d\n", n1, n2, n3);
+
+	return 0;
+}
