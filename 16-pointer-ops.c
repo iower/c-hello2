324b8067c7df4502564e157a1be29007feb2c42b
iower <iower@tutanota.com>
2026-08-17 13:43:31 +0500

    pointer ops

--- 16-pointer-ops.c
+++ 16-pointer-ops.c
@@ -24,6 +24,38 @@ int main(void)
 
 	pa = NULL;
 	printf("pa | %p\n", pa);
-	printf("*pa | %d\n", *pa);
+	// printf("*pa | %d\n", *pa); // segfault
+
+	// void pointer
+	int x = 123;
+	int *ip = &x;
+	void *vp;
+	vp = ip;
+	printf("vp %d\n", *((int *)vp));
+	// printf("vp %d\n", *vp); // cannot
+
+	// pointer address
+	int aa = 10;
+	int *paa = &aa;
+	printf("address of pointer = %p\n", &paa);
+	printf("address in pointer = %p\n", paa);
+	printf("value on pointer = %d\n", *paa);
+
+	// comparsion operations
+	int k = 10;
+	int l = 20;
+	int *pk = &k;
+	int *pl = &l;
+	if (pk > pl)
+		printf("pk (%p) is greater than pl (%p)\n", pk, pl);
+	else
+		printf("pk (%p) is is less or equal pl (%p)\n", pk, pl);
+
+	// type casting
+	char c = "A";
+	char *pc = &c;
+	int *pd = (int *)pc;
+	printf("pc = %p\n", pc);
+	printf("pd = %p\n", pd);
 }
 
