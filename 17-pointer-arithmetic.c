183b3726935b4e723cf3723b306e0068672fdd7b
iower <iower@tutanota.com>
2026-08-20 17:31:19 +0500

    pointer arithmetic

---
+++ 17-pointer-arithmetic.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stddef.h>
+
+int main(void)
+{
+	int n = 10; // 4 b
+
+	int *pn = &n;
+	printf("pn\taddr = %p\t val = %d\n", pn, *pn);
+
+	pn++;
+	printf("pn\taddr = %p\t val = %d\n", pn, *pn);
+
+	pn--;
+	printf("pn\taddr = %p\t val = %d\n\n", pn, *pn);
+
+	double d = 11.1; // 8 b
+	double *pd = &d;
+	printf("pd\taddr = %p\t val = %f\n", pd, *pd);
+	pd++;
+	printf("pd\taddr = %p\t val = %f\n", pd, *pd);
+	pd++;
+	printf("pd\taddr = %p\t val = %f\n", pd, *pd);
+	pd -= 2;
+	printf("pd\taddr = %p\t val = %f\n\n", pd, *pd);
+
+	char c = 'A'; // 1b
+	char *pc = &c;
+	printf("pc\taddr = %p\t val = %c\n", pc, *pc);
+	pc++;
+	printf("pc\taddr = %p\t val = %c\n", pc, *pc);
+
+
+	int a = 10;
+	int b = 11;
+	int *pa = &a;
+	int *pb = &b;
+	ptrdiff_t diff = pa - pb;
+	printf("pa %p\n", pa);
+	printf("pb %p\n", pb);
+	printf("diff %td\n", diff);
+
+
+	int arr[64];
+	int *p1 = &arr[10];
+	int *p2 = &arr[40];
+	ptrdiff_t dist = p2 - p1;
+	printf("dict %td\n", dist);
+
+	return 0;
+}
