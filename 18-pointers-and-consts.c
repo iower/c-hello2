3653e6d350843a1d3581f84dba2852509d688b46
iower <iower@tutanota.com>
2026-08-27 11:51:25 +0500

    pointers and consts

--- 18-pointers-and-consts.c
+++ 18-pointers-and-consts.c
@@ -11,10 +11,40 @@ int main(void)
 	*cx_p = 30;
 	printf("cx = %d\n", cx);
 
+
 	// pointer to const
 	const int a = 10;
 	const int *pa = &a;
-	printf("addr %p\t val %d\n", pa, *pa);
+	printf("pa addr %p\t val %d\n", pa, *pa);
+
+	// *pa = 11; // error: assignment of read-only location ‘*pa’
+
+	int b = 20;
+	const int *pb = &b;
+	printf("pb addr %p\t val %d\n", pb, *pb);
+
+	// *pb = 21; // error: assignment of read-only location ‘*pb’
+
+
+	// const pointer
+	int c = 30;
+	int * const pc = &c;
+	printf("pc addr %p\t val %d\n", pc, *pc);
+
+	*pc = 31;
+	printf("pc addr %p\t val %d\n", pc, *pc);
+
+	// pc = &b; // error: assignment of read-only variable ‘pc’
+
+
+	// const pointer to const
+	int d = 40;
+	const int * const pd = &d;
+
+	// *pd = 41; // error: assignment of read-only location ‘*pd’
+	d = 41;
+	// pd = &b; // error: assignment of read-only variable ‘pd’
+
 
 	return 0;
 }
