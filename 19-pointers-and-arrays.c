b85e658c4958bb2d48ed12d4fa760b71d9761269
iower <iower@tutanota.com>
2026-08-29 12:56:47 +0500

    pointers and arrays

--- 19-pointers-and-arrays.c
+++ 19-pointers-and-arrays.c
@@ -16,4 +16,24 @@ int main(void)
 	printf("%d\n", *(numbers+1));
 	printf("%d\n", numbers[1]);
 	printf("%d\n", 1[numbers]);
+
+	int *ptr = numbers;
+	printf("value: %d\n", *ptr);
+	ptr += 2;
+	printf("value: %d\n", *ptr);
+
+	*ptr = 333;
+	printf("numbers[2] = %d\n", numbers[2]);
+
+	// iterate an array with a pointer
+	for(int *p_n = numbers; p_n <= &numbers[4]; p_n++)
+	{
+		printf("addr %p val %d\n", p_n, *p_n);
+	}
+
+	// iterate multidimensional array
+	int ar[3][4] = {{11, 12, 13, 14}, {21, 22, 23, 24}, {31, 32, 33, 34}};
+
+
+	return 0;
 }
